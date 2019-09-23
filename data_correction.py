ca1dd19d338c5350250e70d0a5b4f989edbddfed
Vmirwald <vitor.mirwald@usp.br>
2019-09-23 17:31:51 -0300

    Atualização e automatização da ferramenta de somar horas

--- data_correction.py
+++ data_correction.py
@@ -9,23 +9,32 @@ Created on Wed Sep  4 16:36:09 2019
 import pandas as pd
 from pandas import DataFrame as df
 
-data = pd.read_csv('eventos1.csv')
+
+arquivo = input('Entre com o nome do arquvivo sem .csv \n')
+campo = input('Entre com o nome da coluna \n')
+
+horas = input("horas a corrigir\n")
+horas = int(horas)
+
+data = pd.read_csv(arquivo)
 
 df = df(data) #df é um DataFrame onde o conteúdo são os dados da tabela Data
 
 
-df['field_eventdate'] = pd.to_datetime(df['field_eventdate'])
-df['field_eventdate'] = pd.to_datetime(df['field_eventdate'], format = '%Y-%b-%d%H:%M') #função que reconhece formato de hora e data
-df['field_eventdate'] = pd.to_datetime(df['field_eventdate'].astype(str)) + pd.DateOffset(hours=3, minutes=00) #Adiciona 3horas 
-df.to_csv('eventos_tratado.csv', index = False) #faz novo arquivo .csv
+df[campo] = pd.to_datetime(df[campo])
+df[campo] = pd.to_datetime(df[campo], format = '%Y-%m-%d %H:%M:s') #função que reconhece formato de hora e data
+df[campo] = pd.to_datetime(df[campo].astype(str)) + pd.DateOffset(hours= horas, minutes=00) #Adiciona 3horas 
+df.to_csv(arquivo + "_data_time_tratado.csv", index = False) #faz novo arquivo .csv
 
 #Alterando campo ' ' para T novamente.
-data = open("eventos_tratado.csv",'r')
-dataout = open("eventos_+3.csv", "w")
+data = open(arquivo + "_data_time_tratado.csv",'r')
+
+horas1 = str(horas)
+dataout = open(arquivo +"_data_time_tratado_"+ horas1 + ".csv", "w")
 
 for line in data:
 	dataout.write(line.replace(' ', 'T'))
-	
+data = dataout
 data.close()
 dataout.close()
 
