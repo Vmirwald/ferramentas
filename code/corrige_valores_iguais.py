bcf930c8789512d1b950278b4d6dd5b1a531b06a
vitor <vitor@sti.sti.fflch.usp.br>
2019-11-08 16:21:11 -0300

    correção do código que trata arquivos duplicados

--- code/corrige_valores_iguais.py
+++ code/corrige_valores_iguais.py
@@ -7,25 +7,27 @@ Created on Fri Sep 27 09:57:00 2019
 """
 
 from pandas import DataFrame as df, read_csv
+import pandas as pd
+import numpy as np
 
 arquivo = input ("Arquivo a ser modificado ( + .csv):\n") #recebe arquivo
 coluna = input("Nome da coluna a ser transformada em linha:\n")#recebe nome da coluna
 
-data = read_csv(arquivo, sep = ',')
+data = read_csv('fornecedores_1.csv', sep = ',')
 
 for i in data['nid'].unique(): #percorre todos os nids
     
     aux = data[data['nid']==i][['nid','title',coluna]] #seleciona apenas os nids que são iguais a i
-    
-    ramo = aux[coluna].to_list() #transforma a coluna 'ramno' em lista
-    
-    title = aux['title'].unique() #pega o primeiro valor de 'title' sem repetição
-    nid = aux['nid'].unique() #pega o primeiro valor de 'nid' sem repetição
-    
-    new = df([ramo],[nid,[title[0]]]) #adiciona nova linha a um dataframe novo
-    
-    new.to_csv('arquivo_tratado.csv', mode = 'a', header = False) #salva novo .csv
-    
+
+    c = aux[coluna].tolist()#transforma a coluna em lista
+    c = df(c)
+    title = df(aux['title'].unique()) #pega o primeiro valor de 'title' sem repetição
+    nid = df(aux['nid'].unique()).astype(object) #pega o primeiro valor de 'nid' sem repetiçãow
+    dat = pd.concat([nid,title,c]).T # concatena os novos dataframes e pega o transposto.
+
+
+
+    dat.to_csv('tratado_'+ arquivo, mode = 'a', header = False, index = False) #salva novo .csv   
 print('\\\*** Arquivo Pronto ***///')
 
 #fim de programa
